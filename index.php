fd54894811cc49b87f0e608eefdcecee4438632f
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-17 16:53:06 -0300

    Commit 1 - Inicializando Chatbot en PHP, Creación en proceso del chat más con pruebas de funcionalidades en JavaScript

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chatbot</title>
+</head>
+<body>
+    <div class="chat-container">
+        <h2>Chatbot PHP-Geminis API</h2>
+        <div id="chat-box"></div>
+        <div class="input-container">
+            <input type="text" id="user-input" placeholder="Escriba su mensaje">
+            <button onclick="enviarMensaje()">Enviar</button>
+        </div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
